e22c047a73541bb943476553528476207ca614d2
JonathanHolmgren <Jonathan.holmgren@hotmail.com>
2024-06-20 23:32:53 +0200

    Add testcontainers dependecy for integration and E2E testing

---
+++ src/test/java/org/fungover/mmotodo/MmotodoEndToEndTest.java
@@ -0,0 +1,45 @@
+package org.fungover.mmotodo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MmotodoEndToEndTest {
+
+
+    @LocalServerPort
+    private int port;
+
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Container
+    private static MySQLContainer container = new MySQLContainer("mysql:latest");
+
+    @DynamicPropertySource
+    public static void override(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+
+    }
+
+   
+
+
+
+}
